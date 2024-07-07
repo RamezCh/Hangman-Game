2be2e11273247e88274b976148354bc71a4f92f3
RamezCh <ramiz_chreide@outlook.com>
2024-07-08 01:31:43 +0300

    Random Word Logic

--- script.js
+++ script.js
@@ -24,3 +24,47 @@ lettersArray.forEach(letter => {
   // Append Span To The Letters Container
   lettersContainer.appendChild(span);
 });
+
+// Object Of Words + Categories
+const words = {
+  programming: [
+    'php',
+    'javascript',
+    'go',
+    'scala',
+    'fortran',
+    'r',
+    'mysql',
+    'python',
+  ],
+  movies: [
+    'shawshank redemption',
+    'godfather',
+    'dark knight',
+    'casablanca',
+    'pulp fiction',
+    'forrest gump',
+    'inception',
+  ],
+  people: [
+    'brad pitt',
+    'angelina jolie',
+    'morgan freeman',
+    'tom hanks',
+    'jack nicholson',
+  ],
+  countries: ['egypt', 'syria', 'yemen', 'australia', 'canada', 'china'],
+};
+
+// Get Random Property
+// This function will take an object and return all of its properties
+let allKeys = Object.keys(words);
+// console.log(allKeys);
+let randomPropNumber = Math.floor(Math.random() * allKeys.length);
+let randomPropName = allKeys[randomPropNumber];
+// console.log(words[randomPropName]);
+let randomPropValue = words[randomPropName];
+// console.log(randomPropValue);
+let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
+let randomValue = randomPropValue[randomValueNumber];
+// console.log(randomValue);
