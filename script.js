c61da023ca6622e887161072c532b45a1246256b
RamezCh <ramiz_chreide@outlook.com>
2024-07-08 17:54:56 +0300

    ReDesign

--- script.js
+++ script.js
@@ -1,154 +1,125 @@
 'use strict';
 
-// Letters
+// Letters and Game Variables
 const letters = 'abcdefghijklmnopqrstuvwxyz';
 const maxWrongAttempts = 8;
-
-// Get Array From Letters
 const lettersArray = Array.from(letters);
 
-// Select Elements
 const lettersContainer = document.querySelector('.letters');
 const categorySpan = document.querySelector('.game-info .category span');
 const lettersGuessContainer = document.querySelector('.letters-guess');
-const draw = document.querySelector('.hangman-draw');
-const popup = document.querySelector('.popup');
+const hangmanDraw = document.querySelector('.hangman-draw');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
 const popupMessage = document.querySelector('.popup-message');
 const playAgainButton = document.querySelector('.play-again');
 
-// Generate Letters
-const generateLetters = () => {
-  lettersArray.forEach(letter => {
-    const span = document.createElement('span');
-    span.textContent = letter;
-    span.className = 'letter-box';
-    lettersContainer.appendChild(span);
-  });
-};
-
-// Game Words and Categories
-const words = {
-  programming: [
-    'php',
-    'javascript',
-    'go',
-    'scala',
-    'fortran',
-    'r',
-    'mysql',
-    'python',
-  ],
-  movies: [
-    'shawshank redemption',
-    'godfather',
-    'dark knight',
-    'casablanca',
-    'pulp fiction',
-    'forrest gump',
-    'inception',
-  ],
-  people: [
-    'brad pitt',
-    'angelina jolie',
-    'morgan freeman',
-    'tom hanks',
-    'jack nicholson',
-  ],
-  countries: ['egypt', 'syria', 'yemen', 'australia', 'canada', 'china'],
-};
-
-// Get Random Word
-const getRandomWord = () => {
-  const categories = Object.keys(words);
-  const category = categories[Math.floor(Math.random() * categories.length)];
-  const word =
-    words[category][Math.floor(Math.random() * words[category].length)];
-  return { category, word };
-};
-
-// Initialize Game
-const initGame = () => {
-  const { category, word } = getRandomWord();
-  categorySpan.textContent = category;
-
-  const lettersAndSpace = Array.from(word);
-
-  lettersAndSpace.forEach(letter => {
-    const span = document.createElement('span');
-    if (letter === ' ') span.className = 'has-space';
-    lettersGuessContainer.appendChild(span);
-  });
-
-  return word;
-};
-
-// Handle Letter Click
-const handleLetterClick = (letter, word, guessSpans) => {
-  let isFound = false;
-  let correctGuessCount = 0;
-
-  // Check if letter exists in the word
-  Array.from(word).forEach((wordLetter, index) => {
-    if (letter === wordLetter) {
-      guessSpans[index].textContent = letter;
-      isFound = true;
-      correctGuessCount++;
-    }
-  });
-
-  // Check if guessed letter is correct or not
-  if (!isFound) {
-    wrongAttempts++;
-    draw.classList.add(`wrong-${wrongAttempts}`);
-    if (wrongAttempts === maxWrongAttempts) endGame(word, false);
-  } else {
-    correctGuesses += correctGuessCount;
-    if (correctGuesses === word.replace(/ /g, '').length) endGame(word, true);
-  }
-};
-
-// End Game
-const endGame = (word, won) => {
-  popupMessage.textContent = won
-    ? `Congratulations! You guessed the word: ${word}`
-    : `Game Over! The word was: ${word}`;
-  popup.style.backgroundColor = won ? '#009688' : '#e74c3c';
-  popup.style.display = 'block';
-  lettersContainer.classList.add('finished');
-};
-
-// Restart Game
-const restartGame = () => {
-  wrongAttempts = 0;
-  correctGuesses = 0;
-  lettersContainer.innerHTML = '';
-  lettersGuessContainer.innerHTML = '';
-  draw.className = 'hangman-draw';
-  popup.style.display = 'none';
-  lettersContainer.classList.remove('finished');
-  generateLetters();
-  word = initGame();
-  guessSpans = document.querySelectorAll('.letters-guess span');
-};
-
-// Main
 let wrongAttempts = 0;
 let correctGuesses = 0;
-let word = initGame();
-let guessSpans = document.querySelectorAll('.letters-guess span');
+
+const words = {
+  programming: ['python', 'javascript', 'php', 'html', 'css'],
+  animals: ['dog', 'cat', 'elephant', 'tiger', 'lion'],
+  countries: ['lebanon', 'france', 'germany', 'brazil', 'canada'],
+};
+
+let randomCategory;
+let randomWord;
+let wordArray;
+let guessSpans;
+
+function initializeGame() {
+  randomCategory = getRandomCategory();
+  randomWord = getRandomWord(randomCategory);
+  wordArray = Array.from(randomWord);
+
+  categorySpan.innerHTML = randomCategory;
+  renderLetters();
+  renderWordSpaces();
+  guessSpans = document.querySelectorAll('.letters-guess span');
+}
+
+function getRandomCategory() {
+  const categories = Object.keys(words);
+  return categories[Math.floor(Math.random() * categories.length)];
+}
+
+function getRandomWord(category) {
+  const wordList = words[category];
+  return wordList[Math.floor(Math.random() * wordList.length)];
+}
+
+function renderLetters() {
+  lettersContainer.innerHTML = '';
+  lettersArray.forEach(letter => {
+    const span = document.createElement('span');
+    span.className = 'letter-box';
+    span.innerHTML = letter;
+    lettersContainer.appendChild(span);
+  });
+}
+
+function renderWordSpaces() {
+  lettersGuessContainer.innerHTML = '';
+  wordArray.forEach(letter => {
+    const span = document.createElement('span');
+    if (letter === ' ') {
+      span.className = 'has-space';
+    }
+    lettersGuessContainer.appendChild(span);
+  });
+}
 
 document.addEventListener('click', e => {
   if (e.target.className === 'letter-box') {
-    const letter = e.target.textContent.toLowerCase();
-    e.target.classList.add('clicked');
-    handleLetterClick(letter, word, guessSpans);
-  }
-  if (e.target === popup) {
-    popup.style.display = 'none';
+    handleLetterClick(e.target);
   }
 });
 
-playAgainButton.addEventListener('click', restartGame);
+function handleLetterClick(target) {
+  target.classList.add('clicked');
+  const clickedLetter = target.innerHTML.toLowerCase();
+  let guessedCorrectly = false;
 
-// Start the game
-generateLetters();
+  wordArray.forEach((wordLetter, wordIndex) => {
+    if (clickedLetter === wordLetter) {
+      guessedCorrectly = true;
+      guessSpans[wordIndex].innerHTML = clickedLetter;
+      correctGuesses++;
+    }
+  });
+
+  if (!guessedCorrectly) {
+    wrongAttempts++;
+    hangmanDraw.classList.add(`wrong-${wrongAttempts}`);
+    if (wrongAttempts === maxWrongAttempts) {
+      endGame(false);
+    }
+  } else if (correctGuesses === wordArray.length) {
+    endGame(true);
+  }
+}
+
+playAgainButton.addEventListener('click', resetGame);
+
+function endGame(won) {
+  modal.classList.add('show');
+  overlay.classList.add('show');
+  popupMessage.innerHTML = won
+    ? 'Congratulations, You Won!'
+    : `Game Over! The word was ${randomWord}`;
+  popupMessage.style.color = won ? '#009688' : '#e74c3c';
+  lettersContainer.classList.add('finished');
+}
+
+function resetGame() {
+  modal.classList.remove('show');
+  overlay.classList.remove('show');
+  hangmanDraw.className = 'hangman-draw';
+  wrongAttempts = 0;
+  correctGuesses = 0;
+  lettersContainer.classList.remove('finished');
+  initializeGame();
+}
+
+initializeGame();
